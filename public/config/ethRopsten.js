41e93db36060d523dd896ab8aadc5ebcc1720c00
Scott Terry <scottterry@MacBook-Pro-2.local>
2022-07-20 17:06:35 -0700

    attempted config fix;

--- public/config/ethRopsten.js
+++ public/config/ethRopsten.js
@@ -1,15 +1,21 @@
-'use strict';
+"use strict";
 
-const LumerinContracts = require('@lumerin/contracts');
-const { Lumerin, CloneFactory, Implementation } = LumerinContracts['ropsten'];
+const LumerinContracts = require("@lumerin/contracts");
+const { Lumerin, CloneFactory, Implementation } = LumerinContracts["ropsten"];
 
-const indexerUrl = process.env.ROPSTEN_INDEXER_URL || 'http://localhost:3005';
-const wsApiUrl = process.env.ROPSTEN_NODE_URL || 'wss://ropsten.infura.io/ws/v3/4b68229d56fe496e899f07c3d41cb08a' || 'ws://localhost:8546';
+const indexerUrl =
+  process.env.ROPSTEN_INDEXER_URL ||
+  "https://indexer.metronome.io" ||
+  "http://localhost:3005";
+const wsApiUrl =
+  process.env.ROPSTEN_NODE_URL ||
+  "wss://ropsten.infura.io/ws/v3/4b68229d56fe496e899f07c3d41cb08a" ||
+  "ws://localhost:8546";
 
 module.exports = {
-  displayName: 'Ropsten',
-  chainId: '3',
-  symbol: 'LMR',
+  displayName: "Ropsten",
+  chainId: "3",
+  symbol: "ETH",
 
   // contract addresses
   lmrTokenAddress: Lumerin.address,
@@ -18,14 +24,17 @@ module.exports = {
   ImplementationAddress: Implementation.address,
 
   // urls
-  proxyRouterUrl: process.env.PROXY_ROUTER_BASE_URL || 'proxyrouter.stg.lumerin.io:8080' || 'localhost:8080',
-  explorerUrl: 'https://ropsten.etherscan.io/tx/{{hash}}',
+  proxyRouterUrl:
+    process.env.PROXY_ROUTER_BASE_URL ||
+    "proxyrouter.stg.lumerin.io:8080" ||
+    "localhost:8080",
+  explorerUrl: "https://ropsten.etherscan.io/tx/{{hash}}",
   indexerUrl,
   wsApiUrl,
 
   // defauls
-  coinDefaultGasLimit: '999999',
-  lmrDefaultGasLimit: '999999',
-  defaultGasPrice: '1000000000',
-  maxGasPrice: '20000000000000000'
+  coinDefaultGasLimit: "999999",
+  lmrDefaultGasLimit: "999999",
+  defaultGasPrice: "1000000000",
+  maxGasPrice: "20000000000000000",
 };
