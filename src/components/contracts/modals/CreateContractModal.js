83e19d8148dcac8416a4810d19cfa28447fe4343
bohdan-titan <bohdank@titan.io>
2023-02-22 17:32:13 +0200

    Changed preview contract page

--- src/components/contracts/modals/CreateContractModal.js
+++ src/components/contracts/modals/CreateContractModal.js
@@ -57,124 +57,128 @@ function CreateContractModal(props) {
     <Modal onClick={handleClose}>
       <Body onClick={handlePropagation}>
         {CloseModal(handleClose)}
-        <TitleWrapper>
-          <Title>Create new contract</Title>
-          <Subtitle>Sell your hashpower on the Lumerin Marketplace</Subtitle>
-        </TitleWrapper>
         {isPreview ? (
           <CreateContractPreview
             data={getValues()}
-            close={handleClose}
+            close={() => setIsPreview(false)}
             submit={handleDeploy}
           />
         ) : (
-          <Form onSubmit={() => setIsPreview(true)}>
-            <Row>
-              <InputGroup>
-                <Label htmlFor="address">Ethereum Address *</Label>
-                <Input
-                  {...register('address', {
-                    required: true,
-                    validate: address => {
-                      /^(0x){1}[0-9a-fA-F]{40}$/i.test(address);
-                    }
-                  })}
-                  readOnly
-                  disable={true}
-                  style={{ cursor: 'default' }}
-                  type="text"
-                  name="address"
-                  id="address"
-                />
-                <Sublabel>
-                  Funds will be paid into this account once the contract is
-                  fulfilled.
-                </Sublabel>
-                {formState?.errors?.address?.type === 'validate' && (
-                  <ErrorLabel>Invalid address</ErrorLabel>
-                )}
-              </InputGroup>
-            </Row>
-            <Row>
-              <InputGroup>
-                <Label htmlFor="time">Time *</Label>
-                <Input
-                  {...register('time', {
-                    required: true,
-                    min: 1
-                  })}
-                  placeholder="# of hours"
-                  type="number"
-                  name="time"
-                  id="time"
-                  min={1}
-                />
-                <Sublabel>Contract Length</Sublabel>
-                {formState?.errors?.time?.type === 'required' && (
-                  <ErrorLabel>Time is required</ErrorLabel>
-                )}
-              </InputGroup>
-            </Row>
-            <Row>
-              <InputGroup>
-                <Label htmlFor="speed">Speed *</Label>
-                <Input
-                  {...register('speed', {
-                    required: true,
-                    min: 1
-                  })}
-                  placeholder="Number of TH/s"
-                  type="number"
-                  name="speed"
-                  id="speed"
-                  min={1}
-                />
-                <Sublabel>Amount of TH/s Contracted</Sublabel>
-                {formState?.errors?.speed?.type === 'required' && (
-                  <ErrorLabel>Speed is required</ErrorLabel>
-                )}
-              </InputGroup>
-            </Row>
-            <Row>
-              <InputGroup>
-                <div>
-                  <Label htmlFor="price">List Price (LMR) *</Label>
-                </div>
-                <Input
-                  {...register('price', {
-                    required: true,
-                    min: 1
-                  })}
-                  placeholder="LMR Charged for Hash Power"
-                  type="number"
-                  name="price"
-                  id="price"
-                  min={1}
-                />
-                <Sublabel>
-                  This is the price you will deploy your contract to the
-                  marketplace.
-                </Sublabel>
-                {formState?.errors?.price?.type === 'required' && (
-                  <ErrorLabel>Price is required</ErrorLabel>
-                )}
-              </InputGroup>
-            </Row>
-            <InputGroup
-              style={{
-                textAlign: 'center',
-                justifyContent: 'space-between',
-                height: '60px'
-              }}
-            >
-              <Row style={{ justifyContent: 'center' }}>
-                {/* <LeftBtn onClick={handleSaveDraft}>Save as Draft</LeftBtn> */}
-                <RightBtn disabled={!formState?.isValid} type="submit">
-                  Create Contract
-                </RightBtn>
+          <>
+            <TitleWrapper>
+              <Title>Create new contract</Title>
+              <Subtitle>
+                Sell your hashpower on the Lumerin Marketplace
+              </Subtitle>
+            </TitleWrapper>
+            <Form onSubmit={() => setIsPreview(true)}>
+              <Row>
+                <InputGroup>
+                  <Label htmlFor="address">Ethereum Address *</Label>
+                  <Input
+                    {...register('address', {
+                      required: true,
+                      validate: address => {
+                        /^(0x){1}[0-9a-fA-F]{40}$/i.test(address);
+                      }
+                    })}
+                    readOnly
+                    disable={true}
+                    style={{ cursor: 'default' }}
+                    type="text"
+                    name="address"
+                    id="address"
+                  />
+                  <Sublabel>
+                    Funds will be paid into this account once the contract is
+                    fulfilled.
+                  </Sublabel>
+                  {formState?.errors?.address?.type === 'validate' && (
+                    <ErrorLabel>Invalid address</ErrorLabel>
+                  )}
+                </InputGroup>
               </Row>
-            </InputGroup>
-          </Form>
+              <Row>
+                <InputGroup>
+                  <Label htmlFor="time">Time *</Label>
+                  <Input
+                    {...register('time', {
+                      required: true,
+                      min: 1
+                    })}
+                    placeholder="# of hours"
+                    type="number"
+                    name="time"
+                    id="time"
+                    min={1}
+                  />
+                  <Sublabel>Contract Length</Sublabel>
+                  {formState?.errors?.time?.type === 'required' && (
+                    <ErrorLabel>Time is required</ErrorLabel>
+                  )}
+                </InputGroup>
+              </Row>
+              <Row>
+                <InputGroup>
+                  <Label htmlFor="speed">Speed *</Label>
+                  <Input
+                    {...register('speed', {
+                      required: true,
+                      min: 1
+                    })}
+                    placeholder="Number of TH/s"
+                    type="number"
+                    name="speed"
+                    id="speed"
+                    min={1}
+                  />
+                  <Sublabel>Amount of TH/s Contracted</Sublabel>
+                  {formState?.errors?.speed?.type === 'required' && (
+                    <ErrorLabel>Speed is required</ErrorLabel>
+                  )}
+                </InputGroup>
+              </Row>
+              <Row>
+                <InputGroup>
+                  <div>
+                    <Label htmlFor="price">List Price (LMR) *</Label>
+                  </div>
+                  <Input
+                    {...register('price', {
+                      required: true,
+                      min: 1
+                    })}
+                    placeholder="LMR Charged for Hash Power"
+                    type="number"
+                    name="price"
+                    id="price"
+                    min={1}
+                  />
+                  <Sublabel>
+                    This is the price you will deploy your contract to the
+                    marketplace.
+                  </Sublabel>
+                  {formState?.errors?.price?.type === 'required' && (
+                    <ErrorLabel>Price is required</ErrorLabel>
+                  )}
+                </InputGroup>
+              </Row>
+              <InputGroup
+                style={{
+                  textAlign: 'center',
+                  justifyContent: 'space-between',
+                  height: '60px'
+                }}
+              >
+                <Row style={{ justifyContent: 'center' }}>
+                  {/* <LeftBtn onClick={handleSaveDraft}>Save as Draft</LeftBtn> */}
+                  <RightBtn disabled={!formState?.isValid} type="submit">
+                    Create Contract
+                  </RightBtn>
+                </Row>
+              </InputGroup>
+            </Form>
+          </>
         )}
       </Body>
     </Modal>
--- src/components/contracts/modals/CreateContractPreview.js
+++ src/components/contracts/modals/CreateContractPreview.js
@@ -1,67 +1,72 @@
 import React from 'react';
-import styled from 'styled-components';
+import {
+  TitleWrapper,
+  Title,
+  Row,
+  RightBtn,
+  LeftBtn
+} from './CreateContractModal.styles';
+import PriceIcon from '../../icons/PriceIcon';
+import SpeedIcon from '../../icons/SpeedIcon';
+import DurationIcon from '../../icons/DurationIcon';
 
-import { Row, RightBtn, LeftBtn } from './CreateContractModal.styles';
-import { truncateAddress } from '../utils';
-
-const ReviewItems = styled.div`
-  div {
-    display: flex;
-    justify-content: space-between;
-    margin: 1.25rem 0;
-    padding-bottom: 1rem;
-    border-bottom: 1px solid #eaf7fc;
-    &:last-child {
-      border-bottom: none;
-    }
-    h4 {
-      font-weight: 100;
-    }
-    p {
-      color: #014353;
-      font-weight: 500;
-    }
-  }
-
-  .total-cost {
-    margin-top: 1rem;
-    display: flex;
-    flex-direction: column;
-    .price {
-      font-size: 2rem;
-    }
-  }
-`;
+import {
+  Divider,
+  HeaderFlex,
+  SmallTitle,
+  Values,
+  UpperCaseTitle,
+  ContractInfoContainer
+} from './PurchaseModal/common.styles';
 
 export const CreateContractPreview = ({
   data: { address, time, speed, price },
   submit,
   close
-}) => {
-  return (
-    <>
-      <ReviewItems>
+}) => (
+  <>
+    <TitleWrapper>
+      <Title>Review Contract</Title>
+      <HeaderFlex>
+        <UpperCaseTitle>SUMMARY</UpperCaseTitle>
+      </HeaderFlex>
+      <Divider />
+      <ContractInfoContainer>
         <div>
-          <h4>Wallet Address</h4>
-          <p>{truncateAddress(address, 'MEDIUM')}</p>
+          <SmallTitle>Speed</SmallTitle>
+          <Values>
+            <SpeedIcon
+              key={'speed'}
+              style={{ marginRight: '4px', height: '1.4rem' }}
+            />
+            {speed} TH/s
+          </Values>
         </div>
         <div>
-          <h4>Contract Time</h4>
-          <p>{time} hours</p>
+          <SmallTitle>Duration</SmallTitle>
+          <Values>
+            <DurationIcon
+              key={'duration'}
+              style={{ marginRight: '4px', height: '1.4rem' }}
+            />
+            {time} hours
+          </Values>
         </div>
         <div>
-          <h4>Speed</h4>
-          <p>{speed} TH/S</p>
+          <SmallTitle>Price</SmallTitle>
+          <Values>
+            <PriceIcon
+              key={'price'}
+              style={{ marginRight: '4px', height: '1.4rem' }}
+            />
+            {price} LMR
+          </Values>
         </div>
-        <div>
-          <h4>List Price</h4>
-          <p>{price} LMR</p>
-        </div>
-      </ReviewItems>
-      <Row>
-        <LeftBtn onClick={close}>Cancel</LeftBtn>
-        <RightBtn onClick={submit}>Confirm New Contract</RightBtn>
-      </Row>
-    </>
-  );
-};
+      </ContractInfoContainer>
+    </TitleWrapper>
+    <Row style={{ marginTop: '3rem' }}>
+      <LeftBtn onClick={close}>Edit Contract</LeftBtn>
+      <RightBtn onClick={submit}>Confirm</RightBtn>
+    </Row>
+  </>
+);
