483d5b5f29c858246025730df85b6cbc01432637
Scott Terry <srt0422@gmail.com>
2023-04-10 09:09:08 -0700

    add recoverMnemonic function

--- public/main/client/keys.js
+++ public/main/client/keys.js
@@ -9,7 +9,12 @@ const isValidMnemonic = mnemonic => bip39.validateMnemonic(mnemonic);
 const mnemonicToSeedHex = mnemonic =>
   bip39.mnemonicToSeedHex(mnemonic).toString('hex');
 
+const recoverMnemonic = (privateKey) => {
+  return bip39.entropyToMnemonic(privateKey.slice(0, 32));
+};
+
 module.exports = {
+  recoverMnemonic,
   createMnemonic,
   isValidMnemonic,
   mnemonicToSeedHex
