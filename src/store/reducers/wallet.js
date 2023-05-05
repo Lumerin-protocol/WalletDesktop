e58e36659ff27e35df221e1f0e26bcb38c273b21
Oleksandr Shevchuk <shev@titan.io>
2023-05-05 14:49:55 +0200

    fix: reducer

--- src/store/reducers/wallet.js
+++ src/store/reducers/wallet.js
@@ -1,7 +1,6 @@
+//@ts-check
 import { handleActions } from 'redux-actions';
 import get from 'lodash/get';
-import keyBy from 'lodash/keyBy';
-import merge from 'lodash/merge';
 
 export const initialState = {
   syncStatus: 'up-to-date',
@@ -26,18 +25,31 @@ export const initialState = {
  */
 const mergeTransactions = (stateTxs, payloadTxs) => {
   const txWithReceipts = payloadTxs.filter(tx => tx.receipt);
-  const txMap = keyBy(txWithReceipts, item => item.transaction.hash);
-
   const newStateTxs = { ...stateTxs };
 
   for (const tx of txWithReceipts) {
-    newStateTxs[tx.transaction.hash] = tx;
+    const oldStateTx = stateTxs[tx.transaction.hash];
 
+    const isDifferentLogIndex =
+      oldStateTx?.transaction?.logIndex &&
+      tx?.transaction?.logIndex &&
+      oldStateTx?.transaction?.logIndex !== tx?.transaction?.logIndex; // means that this is a second transaction within the same hash
+
+    if (oldStateTx && !isDifferentLogIndex) {
+      continue;
+    }
+
+    newStateTxs[tx.transaction.hash] = tx;
     // contract purchase emits 2 transactions with the same hash
     // as of now we merge corresponding amount values. Temporary fix, until refactoring trasactions totally
-    const oldStateTx = stateTxs[tx.transaction.hash];
-    if (oldStateTx) {
-      if (newStateTxs[tx.transaction.hash].transaction.value) {
+
+    // we sum transaction value if it is transfers within the same transaction, but with different logIndex
+    // TODO: display both transactions in the UI either separately or as a single one with two outputs
+    if (oldStateTx && isDifferentLogIndex) {
+      if (
+        newStateTxs[tx.transaction.hash].transaction.value &&
+        oldStateTx.transaction.logIndex !== tx.transaction.logIndex
+      ) {
         newStateTxs[tx.transaction.hash].transaction.value = String(
           Number(oldStateTx.transaction.value) + Number(tx.transaction.value)
         );
