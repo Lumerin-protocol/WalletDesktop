35ca74e25ff67cab09abcfc1045f598c5234a1e2
Aleksandr Kukharenko <aleksandr@titan.io>
2023-05-24 13:53:05 +0300

    add new resource

--- public/main/client/proxyRouter/windows/service.js
+++ public/main/client/proxyRouter/windows/service.js
@@ -9,7 +9,7 @@ const PROXY_ROUTER_MODE = {
 };
 
 const getInstallNssmServiceCommand = (resourcePath) => {
-    return [`wget -O ${resourcePath}/nssm.zip http://nssm.cc/release/nssm-2.24.zip`, `Expand-Archive -LiteralPath ${resourcePath}/nssm.zip`, `${resourcePath}\\nssm\\nssm-2.24\\win64\\nssm.exe install`].join(' && ');
+    return [`Expand-Archive -LiteralPath ${resourcePath}/nssm.zip`, `${resourcePath}\\nssm\\nssm-2.24\\win64\\nssm.exe install`].join(' && ');
 }
 
 const getInstallServiceCommand = (serviceName, pathToExecutable, resourcePath) => {
