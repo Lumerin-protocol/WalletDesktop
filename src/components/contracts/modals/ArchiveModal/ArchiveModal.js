341660309f8824a6073ef151d433b7a01bc72131
bohdan-titan <bohdank@titan.io>
2023-06-13 14:44:44 +0300

    Added call to core

--- src/components/contracts/modals/ArchiveModal/ArchiveModal.js
+++ src/components/contracts/modals/ArchiveModal/ArchiveModal.js
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import withCreateContractModalState from '../../../../store/hocs/withCreateContractModalState';
-import { withRouter } from 'react-router-dom';
 import {
   Modal,
   Body,
@@ -10,9 +8,10 @@ import {
   CloseModal
 } from '../CreateContractModal.styles';
 import ArchiveRow from './ArchiveRow';
+import { withClient } from '../../../../store/hocs/clientContext';
 
 function ArchiveModal(props) {
-  const { isActive, close, deletedContracts } = props;
+  const { isActive, close, deletedContracts, client } = props;
 
   const handleClose = e => {
     close(e);
@@ -23,6 +22,19 @@ function ArchiveModal(props) {
     return <></>;
   }
 
+  const handleRestore = contract => {
+    client.lockSendTransaction();
+    return client
+      .setDeleteContractStatus({
+        contractId: contract.id,
+        walletAddress: contract.seller,
+        deleteContract: false
+      })
+      .finally(() => {
+        client.unlockSendTransaction();
+      });
+  };
+
   return (
     <Modal onClick={handleClose}>
       <Body onClick={handlePropagation}>
@@ -31,10 +43,12 @@ function ArchiveModal(props) {
           <Title>Archived contracts</Title>
         </TitleWrapper>
         {deletedContracts &&
-          deletedContracts.map(c => <ArchiveRow contract={c} />)}
+          deletedContracts.map(c => (
+            <ArchiveRow key={c.id} contract={c} handleRestore={handleRestore} />
+          ))}
       </Body>
     </Modal>
   );
 }
 
-export default withRouter(withCreateContractModalState(ArchiveModal));
+export default withClient(ArchiveModal);
